9d08352f58c0ea6a68237725e5ab0fa47b28af90
Anuj Jindal <anuj.jindal@wayforward.io>
2023-01-23 14:42:32 +0530

    add one more secret

--- misc.sh
+++ misc.sh
@@ -2,3 +2,4 @@
 # aws alias.
 alias aws-gsv='aws secretsmanager get-secret-value --secret-id'
 alias aws-ds='aws secretsmanager describe-secret --secret-id'
+alias aws-ssm-gpv='aws ssm get-parameter --name'
