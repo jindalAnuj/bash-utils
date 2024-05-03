ac8bbd6e9dec1a6b94b60bc3f167ad2da2c4ac6a
Anuj Jindal <31108317+jindalAnuj@users.noreply.github.com>
2024-05-03 12:40:55 +0530

    Add python env commands

--- misc.sh
+++ misc.sh
@@ -3,3 +3,11 @@
 alias aws-gsv='aws secretsmanager get-secret-value --secret-id'
 alias aws-ds='aws secretsmanager describe-secret --secret-id'
 alias aws-ssm-gpv='aws ssm get-parameter --name'
+
+
+
+alias penv="python3 -m venv .venv"
+alias pact="source .venv/bin/activate"
+alias pdeact="deactivate"
+alias pinst="pip install -r requirements.txt"
+alias pfreeze="pip freeze > requirements.txt"
